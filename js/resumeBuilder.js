49a73efc581e53c85407d33a013bc708af92595e
Brett <breifers@gmail.com>
2016-01-06 21:18:57 -0800

    Test

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,8 +1,8 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- var formattedName = "Brett Reifers";
- var formattedRole = "Technical Product Manager";
- HTMLheaderName.replace("%data%", formattedName);
- HTMLheaderRole.replace("%data%", formattedRole);
- $("#main") .append(formattedName) .append(formattedRole);
+ var formattedName = HTMLheaderName.replace("%data%", name);
+ var formattedRole = HTMLheaderRole.replace("%data%", role);
+  
+ $("#header").append(formattedName);
+ $("#header").append(formattedRole);
