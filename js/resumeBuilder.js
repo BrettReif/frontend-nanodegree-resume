94246a1954e5b7d796eef203abc30e14691a47d6
Brett <breifers@gmail.com>
2016-01-06 23:06:22 -0800

    test

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,13 +6,14 @@ This is empty on purpose! Your code to build the resume will go here.
  var formattedName = HTMLheaderName.replace("%data%", name);
  var formattedRole = HTMLheaderRole.replace("%data%", role);
  var skills = ["HTML", "CSS", "SQL", "JavaScript"];
+ var picture = "images/me.jpg"
 
  var bio = {
 	"name" : name,
 	"role" : role,
 	"email" : "breifers@gmail.com",
  	"phone" : "206-788-5705",
- 	"picture" : "images/me.jpg",
+ 	"picture" : picture,
 	"message" : "Here's a bit about me.",
 	"skills" : skills.join(", ")
 };
