36f523029967b96809411091be66727a8297c964
Brett <breifers@gmail.com>
2016-01-06 20:52:00 -0800

    Test

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,5 +3,6 @@ This is empty on purpose! Your code to build the resume will go here.
  */
  $("#main") .append("Brett Reifers");
  var awesomeThoughts = "I am Brett and I am AWESOME!";
- awesomeThoughts.replace("AWESOME", "FUN");
+ var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+
  $("#main") .append(funThoughts);
