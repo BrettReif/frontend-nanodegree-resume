521692a1d6dc322a69763c09b1de144a9246206e
Brett <breifers@gmail.com>
2016-01-06 21:21:50 -0800

    test

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,5 +6,5 @@ This is empty on purpose! Your code to build the resume will go here.
  var formattedName = HTMLheaderName.replace("%data%", name);
  var formattedRole = HTMLheaderRole.replace("%data%", role);
   
- $("#header").append(formattedName);
- $("#header").append(formattedRole);
+ $("#header").prepend(formattedRole);
+ $("#header").prepend(formattedName);
